c8094b5ee10642d6185bb6ed733dc45211de805a
aidanwills <60717604+aidanwills@users.noreply.github.com>
2022-12-05 19:06:27 -0500

    Update make_dicts.py
    Final version  used to generate .txt files

--- make_dicts.py
+++ make_dicts.py
@@ -6,38 +6,41 @@ from urllib.parse import parse_qs
 from unidecode import unidecode
 from time import sleep
 
-_url = 'https://www.ezglot.com/words-starting-with.php' #the international wordle dictionary I'm referencing
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-languages = ['eng', 'fra', 'ita', 'lat', 'por', 'spa']
-all_urls = [] 
 
-'''
-The website lists all of the 5 letter words as links to their own dictionary pages,
-so I just search for those urls and snag the word parameter from it
-There's probably a much cleaner way to do it but I'm pretty inexperienced with this stuff
-'''
-
-while len(all_urls) == 0:
-    #loop in case letter/country combo doesn't have any existing words
-    letter = alphabet[random.randint(0,25)] #pick random letter
-    language = random.choice(languages) #pick random language
-    _params = {'w' : letter,'l' : language, 'length' : '5'}
-    r = requests.get(url = _url, params = _params)
+def good_word(word, lang):
+    #looks up a word's dictionary page -- returns True if there is a valid definition. False otherwise
+    _url = 'https://www.ezglot.com/words.php?l=' + lang + '&w=' + word
+    r = requests.get(url = _url)
     data = BeautifulSoup(r.content, 'html5lib') 
-    all_urls = data.find("body").find_all("ul")[1].find_all('a', href=True) #it's ugly, but this line returns all of the dictionary page links
+    if data.find('h4', id='definitions') != None:
+        return True
+    return False
+
+if __name__ == '__main__':
     
-print('Finding all 5 letter words in ' + language + ' that start with ' + letter + '\n')
-sleep(2)
-
-for url in all_urls:
-    parsed_url = urlparse(url['href']) #parse the url to find the actual word name
-    word = parse_qs(parsed_url.query)['w'][0]
-    print(unidecode(word)) 
-    '''
-    unidecode removes the accents from all of the letters.
-    We can keep the accents on words if we want to display them, 
-    but at some point in the process we'll need to use this function to compare letters and whatnot
-    '''
-
-
+    _url = 'https://www.ezglot.com/words-starting-with.php' #the international wordle dictionary I'm referencing
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    languages = ['eng', 'fra', 'ita', 'lat', 'por', 'spa']
     
+    for language in languages:
+        with open(language + '.txt', 'w') as fptr:
+            #create a .txt file with in the local path
+            for letter in alphabet:
+                print(language  + ' : ' + letter) #keep track of where the program is
+                
+                '''
+                The website lists all of the 5 letter words as links to their own dictionary pages,
+                so I just search for those urls and snag the word parameter from it
+                There's probably a much cleaner way to do it but I'm pretty inexperienced with this stuff
+                '''
+                _params = {'w' : letter,'l' : language, 'length' : '5'}
+                r = requests.get(url = _url, params = _params)
+                data = BeautifulSoup(r.content, 'html5lib') 
+                all_urls = data.find('body').find_all('ul')[1].find_all('a', href=True) #it's ugly, but this line returns all of the dictionary page links
+                if len(all_urls) == 0:
+                    continue 
+                for url in all_urls:
+                    parsed_url = urlparse(url['href']) #parse the url to find the actual word name
+                    word = parse_qs(parsed_url.query)['w'][0]
+                    if good_word(word,language):
+                        fptr.write(unidecode(word) + '\n') #unidecode gets rid of accents
