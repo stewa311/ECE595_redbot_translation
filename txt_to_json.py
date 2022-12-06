186d7b17cb248da5cd40795c3a1f5b2e7c0ba99e
aidanwills <60717604+aidanwills@users.noreply.github.com>
2022-12-06 15:25:53 -0500

    Update txt_to_json.py
    Updated to remove duplicate words and words on list of popular baby names

--- txt_to_json.py
+++ txt_to_json.py
@@ -1,9 +1,27 @@
 import json
 
-langs = ['eng', 'fra', 'ita', 'lat', 'por', 'spa']
-for lang in langs:
-    with open(lang + '.txt', 'r') as fptr:
-        words = fptr.read().splitlines()
+def filter_names(words,names):
+    #filter out any of the words that are just names
+    _words = []
+    for word in words:
+        if word not in names:
+            _words.append(word)
+    return _words
+
+if __name__ == '__main__':
+    with open('names.txt') as fptr:
+        #names.txt is a filtered version of the list of all 2021 baby names provided by the social security administration
+        names = set(fptr.read().splitlines()) 
+
+
+    langs = ['eng', 'fra', 'ita', 'lat', 'por', 'spa']
+    for lang in langs:
+        with open(lang + '.txt', 'r') as fptr:
+            words = fptr.read().splitlines()
+
+        #remove duplicate words
+        words = list(set(words))
+        words = filter_names(words,names)
         
-    with open(lang+'.json', 'w') as fptr:
-        json.dump(words, fptr)
+        with open(lang+'.json', 'w') as fptr:
+            json.dump(words, fptr)
